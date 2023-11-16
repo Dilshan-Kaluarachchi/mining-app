bb26427152020143236809eac98ced18d02a0cae
Dilshan-Kaluarachchi <dilshan.kaluarachchi8@gmail.com>
2023-11-16 09:17:56 -0800

    server.js updates

--- server/server.js
+++ server/server.js
@@ -34,6 +34,25 @@ app.get('/api/miningStatistics', (req, res) => {
   }
 });
 
+// Endpoint to retrieve Bitcoin price
+app.get('/api/bitcoin', (req, res) => {
+  // Assuming you have a variable named bitcoinPrice containing the current Bitcoin price
+  const bitcoinPrice = 45000; // TODO: Need to replace with actual value
+  res.json({ bitcoinPrice });
+});
+
+// Endpoint to retrieve Antminer details
+app.get('/api/antminer', (req, res) => {
+  try {
+    const antminerDataPath = path.join(__dirname, 'Data', 'antminerData.json');
+    const antminerData = JSON.parse(fs.readFileSync(antminerDataPath, 'utf8'));
+    res.json(antminerData);
+  } catch (error) {
+    console.error('Error reading Antminer data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
